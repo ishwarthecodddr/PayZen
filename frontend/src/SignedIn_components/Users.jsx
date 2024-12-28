fbd4b599d8e5d66a729b7344e157265ff3468a09
ishwarthecodddr <ishwarthecoder@gmail.com>
2024-12-28 15:17:10 +0530

    basic app done

--- frontend/src/SignedIn_components/Users.jsx
+++ frontend/src/SignedIn_components/Users.jsx
@@ -1,54 +1,72 @@
-import { useState } from "react"
-import { Button } from "../components/Button"
-
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 export const Users = () => {
-    // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Ishwar",
-        lastName: "",
-        _id: 1
-    }, {
-        firstName: "Gaurav",
-        _id:2
-    },{
-        firstName: "Pratik",
-        _id:3
-        }, {
-        firstName: "Kumar",
-        lastName: "Gaurav",
-        _id:4
-    }]);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
+    fetchUsers();
+  }, [filter]);
+  async function fetchUsers() {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/api/v1/user/bulk?filter=" +
+          encodeURIComponent(filter) // encodeURIComponent is used to encode the filter string so that it can be used in the URL to take care of special characters
+      );
+      setUsers(res.data.user);
+    } catch (error) {
+      console.log(error);
+      setUsers([]); // Keep it as an array even on failure
+    }
+  }
 
-    return <>
-        <div className="font-bold mt-6 text-lg">
-            Users
-        </div>
-        <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
-        </div>
-        <div>
-            {users.map(user => <User user={user} />)}
-        </div>
+  return (
+    <>
+      <div className="font-bold mt-6 text-lg">Users</div>
+      <div className="my-2">
+        <input
+          type="text"
+          placeholder="Search users..."
+          className="w-full px-2 py-1 border rounded border-slate-200"
+          onChange={(e) => setFilter(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        {users.map((user) => (
+          <User user={user} />
+        ))}
+      </div>
     </>
-}
+  );
+};
 
-function User({user}) {
-    return <div className="flex justify-between">
-        <div className="flex">
-            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center h-full text-xl">
-                    {user.firstName[0]}
-                </div>
-            </div>
-            <div className="flex flex-col justify-center h-ful">
-                <div>
-                    {user.firstName} {user.lastName}
-                </div>
-            </div>
+function User({ user }) {
+  const navigate = useNavigate();
+  return (
+    <div className="flex justify-between">
+      <div className="flex">
+        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+          <div className="flex flex-col justify-center h-full text-xl">
+            {user.firstName[0]}
+          </div>
         </div>
-
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+          <div>
+            {user.firstName} {user.lastName}
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-col justify-center h-ful">
+        <Button
+          label={"Send Money"}
+          onPress={() => {
+            navigate("/send/?userId=" + user._id + "&name=" + user.firstName);
+          }}
+        />
+      </div>
     </div>
+  );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,34 @@
-import {Appbar} from '../SignedIn_components/Appbar'
-import {Balance} from '../SignedIn_components/Balance'
-import {Users} from '../SignedIn_components/Users'
+import axios from "axios";
+import { Appbar } from "../SignedIn_components/Appbar";
+import { Balance } from "../SignedIn_components/Balance";
+import { Users } from "../SignedIn_components/Users";
+import { useEffect, useState } from "react";
 export default function Dashboard() {
-    return <div>
-        <Appbar></Appbar>
-        <div className='m-8'>
-            <Balance balance={"1000"}></Balance>
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+          }
+        });
+        setBalance(response.data.balance);
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+      }
+    };
+    fetchBalance();
+  }, []);
+
+  return (
+    <div>
+      <Appbar></Appbar>
+      <div className="m-8">
+        <Balance balance={balance}></Balance>
         <Users></Users>
-        </div>
-        
+      </div>
     </div>
+  );
 }
